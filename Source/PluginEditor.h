87528c237b5b79c85590a83caa8b31ea3ae37e32
m-aiken <mattaiken91@gmail.com>
2022-01-08 00:38:00 +0000

    added a mono meter

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,6 +12,29 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
+struct Meter : juce::Component
+{
+    void paint(juce::Graphics& g) override
+    {
+        auto bounds = getLocalBounds();
+        auto h = bounds.getHeight();
+        
+        g.setColour(juce::Colours::lightblue);
+        // gainToDecibels default range: 0.f to -100.f (default negativeInf)
+        auto jmap = juce::jmap<float>(level, -100.f, 0.f, h, 0);
+        g.fillRect(bounds.withHeight(h * jmap).withY(jmap));
+    }
+    
+    void update(float& newLevel)
+    {
+        level = juce::Decibels::gainToDecibels(newLevel);
+        repaint();
+    }
+    
+private:
+    float level{0};
+};
+//==============================================================================
 /**
 */
 class PFMProject10AudioProcessorEditor  : public juce::AudioProcessorEditor, juce::Timer
@@ -25,16 +48,15 @@ public:
     void resized() override;
     
     void timerCallback() override;
-
+    
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     PFMProject10AudioProcessor& audioProcessor;
     
-    juce::AudioBuffer<float> buffer;
-
-    // to test timer callback / pulling from fifo
-    int timerTestInt = 1000;
+    juce::AudioBuffer<float> incomingBuffer;
     
+    Meter monoMeter;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PFMProject10AudioProcessorEditor)
 };
